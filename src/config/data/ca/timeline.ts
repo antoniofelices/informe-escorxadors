43434f3b74c54a66cb2ad1dfabff42ba605cf2e9
antoniofelices <antonio@studiomoare.com>
2025-12-05 20:22:57 +0100

    Fix timeline

--- src/config/data/ca/timeline.ts
+++ src/config/data/ca/timeline.ts
@@ -1,66 +1,78 @@
-import escorxadormunicipal from '@assets/images/escorxador-municipal.svg'
+import exampleImage from '@assets/images/1920x1080.jpg';
+import ecologicPig from '@assets/images/infographic/ecologic-pig.svg';
 
 const dataTimelineCa = [
-    {
-        image: escorxadormunicipal,
-        altImage: '',
-        title: 'Origen comunal',
-        time: 'Edat Mitjana (S. XIII - XVIII)',
-        description1: 'Model econòmic',
-        text1: 'Economia moral d’ús i reciprocitat',
-        description2: 'Descripció',
-        text2: 'Els escorxadors són mitjans comunals de producció. La propietat és col·lectiva, l’ús és equitatiu i es gestiona pel comú a través d’arrendaments',
-        description3: 'Exemple històric il·lustratiu',
-        text3: 'Carta de Població de Figueres (1267) — el macèl·lum com a bé comunal',
-    },
-    {
-        image: escorxadormunicipal,
-        altImage: '',
-        title: 'La ruptura liberal amb la desamortització',
-        time: '1855 - 1890',
-        description1: 'Model econòmic',
-        text1: "La propietat privada i el mercat lliure vertebren l’economia. Es mercantilitza la terra i la mà d'obra",
-        description2: 'Descripció',
-        text2: 'Es privatitzen terres i serveis (forns, molins, escorxadors) transformant-los, juntament amb la mà d’obra, en mercaderies. Amb la pèrdua d’autonomia de la pagesia, augmenta el conflicte social i la repressió',
-        description3: 'Exemple històric il·lustratiu',
-        text3: 'La Llei de Madoz (1855) destrueix la propietat comunal posant en venda els béns i terres municipals que eren comunals',
-    },
-    {
-        image: escorxadormunicipal,
-        altImage: '',
-        title: 'Servei públic municipal',
-        time: '1890',
-        description1: 'Model econòmic',
-        text1: 'Gestió pública municipal',
-        description2: 'Descripció',
-        text2: 'Els escorxadors esdevenen serveis municipals essencials i pràcticament tots els pobles se’n doten d’un de públic. Són equipaments referents en higiene, arquitectònicament destacats i accessibles',
-        description3: 'Exemple històric il·lustratiu',
-        text3: 'Llei de Bases de Règim Local (1945) assumeix la competència dels escorxadors com a pròpia dels ajuntaments',
-    },
-    {
-        image: escorxadormunicipal,
-        altImage: '',
-        title: 'Privatització i declivi',
-        time: '1996 - 2022',
-        description1: 'Model econòmic',
-        text1: 'Oligopoli privat i dependència logística',
-        description2: 'Descripció',
-        text2: 'Es tanquen 103 escorxadors municipals entre 1998 i 2022. El sacrifici es concentra en macroplantes industrials, deixant només el 2% en escorxadors de baixa capacitat',
-        description3: 'Exemple històric il·lustratiu',
-        text3: 'El Reial Decret-Llei 7/1996 i la Llei Montoro (2013) posen fi a l’obligatorietat del servei de l’equipament escorxador com a servei públic',
-    },
-    {
-        image: escorxadormunicipal,
-        altImage: '',
-        title: 'Cap a un model públic-comunitari',
-        time: '2022 - futur',
-        description1: 'Model econòmic',
-        text1: 'Governança democràtica i economia social',
-        description2: 'Descripció',
-        text2: 'L’escorxador és una infraestructura de sobirania pel repte de la transició ecosocial. Reconeixement del dret a les infraestructures com a clau de la sobirania alimentària',
-        description3: 'Exemple històric il·lustratiu',
-        text3: 'Emergència d’escorxadors públic-comunitaris com l’Escorxador de l’Armentera',
-    },
-]
+  {
+    image: ecologicPig,
+    altImage: '',
+    title: 'Origen comunal',
+    time: 'Edat Mitjana (S. XIII - XVIII)',
+    description1: 'Model econòmic',
+    text1: 'Economia moral d’ús i reciprocitat',
+    description2: 'Descripció',
+    text2:
+      'Els escorxadors són mitjans comunals de producció. La propietat és col·lectiva, l’ús és equitatiu i es gestiona pel comú a través d’arrendaments',
+    description3: 'Exemple històric il·lustratiu',
+    text3:
+      'Carta de Població de Figueres (1267) — el macèl·lum com a bé comunal',
+  },
+  {
+    image: exampleImage,
+    altImage: '',
+    title: 'La ruptura liberal amb la desamortització',
+    time: '1855 - 1890',
+    description1: 'Model econòmic',
+    text1:
+      "La propietat privada i el mercat lliure vertebren l’economia. Es mercantilitza la terra i la mà d'obra",
+    description2: 'Descripció',
+    text2:
+      'Es privatitzen terres i serveis (forns, molins, escorxadors) transformant-los, juntament amb la mà d’obra, en mercaderies. Amb la pèrdua d’autonomia de la pagesia, augmenta el conflicte social i la repressió',
+    description3: 'Exemple històric il·lustratiu',
+    text3:
+      'La Llei de Madoz (1855) destrueix la propietat comunal posant en venda els béns i terres municipals que eren comunals',
+  },
+  {
+    image: exampleImage,
+    altImage: '',
+    title: 'Servei públic municipal',
+    time: '1890',
+    description1: 'Model econòmic',
+    text1: 'Gestió pública municipal',
+    description2: 'Descripció',
+    text2:
+      'Els escorxadors esdevenen serveis municipals essencials i pràcticament tots els pobles se’n doten d’un de públic. Són equipaments referents en higiene, arquitectònicament destacats i accessibles',
+    description3: 'Exemple històric il·lustratiu',
+    text3:
+      'Llei de Bases de Règim Local (1945) assumeix la competència dels escorxadors com a pròpia dels ajuntaments',
+  },
+  {
+    image: exampleImage,
+    altImage: '',
+    title: 'Privatització i declivi',
+    time: '1996 - 2022',
+    description1: 'Model econòmic',
+    text1: 'Oligopoli privat i dependència logística',
+    description2: 'Descripció',
+    text2:
+      'Es tanquen 103 escorxadors municipals entre 1998 i 2022. El sacrifici es concentra en macroplantes industrials, deixant només el 2% en escorxadors de baixa capacitat',
+    description3: 'Exemple històric il·lustratiu',
+    text3:
+      'El Reial Decret-Llei 7/1996 i la Llei Montoro (2013) posen fi a l’obligatorietat del servei de l’equipament escorxador com a servei públic',
+  },
+  {
+    image: exampleImage,
+    altImage: '',
+    title: 'Cap a un model públic-comunitari',
+    time: '2022 - futur',
+    description1: 'Model econòmic',
+    text1: 'Governança democràtica i economia social',
+    description2: 'Descripció',
+    text2:
+      'L’escorxador és una infraestructura de sobirania pel repte de la transició ecosocial. Reconeixement del dret a les infraestructures com a clau de la sobirania alimentària',
+    description3: 'Exemple històric il·lustratiu',
+    text3:
+      'Emergència d’escorxadors públic-comunitaris com l’Escorxador de l’Armentera',
+  },
+];
 
-export default dataTimelineCa
+export default dataTimelineCa;
